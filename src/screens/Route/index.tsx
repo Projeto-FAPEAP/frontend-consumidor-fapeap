3e75da67943acdc2f960dc88e25a64fca2464986
Matheus Costa <math.cs.ceil@gmail.com>
2020-09-08 17:13:03 -0300

    correções ios

--- src/screens/Route/index.tsx
+++ src/screens/Route/index.tsx
@@ -1,58 +1,74 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, Dimensions } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 
+import { useTheme } from 'styled-components';
+
+import house from '../../assets/house.png';
 import logo from '../../assets/icone1024x1024.png';
 
+const GOOGLE_MAPS_APIKEY = 'AIzaSyAJLnjbwqDj7XpSoB7MORWcQMePWUPQ99c';
+
 const Route: React.FC = () => {
+  const { colors } = useTheme();
+
+  const [initial, setInitial] = React.useState({
+    latitude: 0.032472,
+    longitude: -51.062987,
+    latitudeDelta: 0.00522,
+    longitudeDelta:
+      (Dimensions.get('window').width / Dimensions.get('window').height) *
+      0.00522,
+  });
+
+  const [final, setFinal] = React.useState({
+    latitude: 0.0323312,
+    longitude: -51.0711329,
+    latitudeDelta: 0.00522,
+    longitudeDelta:
+      (Dimensions.get('window').width / Dimensions.get('window').height) *
+      0.00522,
+  });
+
   return (
     <MapView
       style={{ flex: 1 }}
-      initialRegion={{
-        latitude: 51.5078788,
-        longitude: -0.0877321,
-        latitudeDelta: 0.05,
-        longitudeDelta: 0.05,
-      }}
-      region={{
-        latitude: 51.5078788,
-        longitude: -0.0877321,
-        latitudeDelta: 0.05,
-        longitudeDelta: 0.05,
-      }}
-      followsUserLocation
+      initialRegion={initial}
+      region={initial}
       zoomEnabled
-      showsUserLocation
     >
-      <Marker coordinate={{ latitude: 51.5078788, longitude: -0.0877321 }} />
+      <MapViewDirections
+        origin={initial}
+        destination={final}
+        apikey={GOOGLE_MAPS_APIKEY}
+        strokeWidth={5}
+        strokeColor={colors.primary}
+      />
 
       <Marker
-        coordinate={{
-          latitude: 51.5078788,
-          longitude: -0.0877321,
-        }}
+        coordinate={initial}
         title="Casa do Açaí"
         description="Inicio do Percurso"
       >
         <Image
-          resizeMode="contain"
+          resizeMode="center"
+          resizeMethod="resize"
           source={logo}
           style={{ height: 60, width: 60 }}
         />
       </Marker>
 
       <Marker
-        coordinate={{
-          latitude: 52.5078788,
-          longitude: -0.0877321,
-        }}
+        coordinate={final}
         title="Casa do Cliente"
         description="Fim do Percurso"
       >
         <Image
-          resizeMode="contain"
-          source={logo}
-          style={{ height: 60, width: 60 }}
+          resizeMode="center"
+          resizeMethod="resize"
+          source={house}
+          style={{ height: 60, width: 60, borderRadius: 50 }}
         />
       </Marker>
     </MapView>
